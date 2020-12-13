6dce2758e5e663310fa6fcc3d32fb09acab18ead
Huseyn <huseynvaliyev2015@gmail.com>
2020-12-13 16:17:06 +0300

    getting text firebase remote config and show on splash screen done

---
+++ ScoutiumInternCase/TableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TableViewController.swift
+//  ScoutiumInternCase
+//
+//  Created by Huseyn Valiyev on 13.12.2020.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- ScoutiumInternCase/ViewController.swift
+++ ScoutiumInternCase/ViewController.swift
@@ -6,13 +6,19 @@
 //
 
 import UIKit
+import FirebaseRemoteConfig
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var remoteText: UILabel!
+    private let remoteConfig = RemoteConfig.remoteConfig()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         if !NetworkMonitor.shared.isConneted {
             showAlert()
+        }else{
+            fetchValues()
         }
     }
     
@@ -27,6 +33,41 @@ class ViewController: UIViewController {
             dialogMessage.addAction(ok)
             self.present(dialogMessage, animated: true, completion: nil)
         }
+    }
+    
+    private func fetchValues() {
+        let defaults: [String: NSObject] = [
+            "text": "Text" as NSObject
+        ]
+        
+        remoteConfig.setDefaults(defaults)
+        
+        let settings = RemoteConfigSettings()
+        settings.minimumFetchInterval = 0
+        remoteConfig.configSettings = settings
+        
+        self.remoteConfig.fetch(withExpirationDuration: 0, completionHandler: {status, error in
+            if status == .success, error == nil {
+                self.remoteConfig.activate() { (changed, error) in
+                    guard error == nil else {
+                        return
+                    }
+                    let value = self.remoteConfig.configValue(forKey: "text").stringValue!
+                    print("Fetched value:\(value)")
+                    DispatchQueue.main.async{
+                        self.remoteText.text = value
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3, execute: {
+                        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                        let tableViewController = storyboard.instantiateViewController(withIdentifier: "table") as! TableViewController
+                        tableViewController.modalPresentationStyle = .fullScreen
+                        self.present(tableViewController, animated: true, completion: nil)
+                    })
+                }
+            }else{
+                print("Fetching Error")
+            }
+        })
     }
 }
 
