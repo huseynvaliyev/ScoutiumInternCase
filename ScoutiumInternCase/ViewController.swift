7c6d95d9e6c1947af3fc4c8ec0496e547c147982
Huseyn <huseynvaliyev2015@gmail.com>
2020-12-13 15:27:09 +0300

    Initial Commit

---
+++ ScoutiumInternCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScoutiumInternCase
+//
+//  Created by Huseyn Valiyev on 13.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
